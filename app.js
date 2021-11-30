e9443426ea232b6c68d22b84806dbf96f19f9fab
lagra-iku <lagra2001@gmail.com>
2021-11-30 23:45:45 +0100

    connection string for db

--- app.js
+++ app.js
@@ -21,27 +21,13 @@ app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || "postgres://lgzdcorcxbvrgv:d4ed8ed76ccfe15034e68dc5c7f13423d10ba2598997673571023783923e0783@ec2-34-241-19-183.eu-west-1.compute.amazonaws.com:5432/deemmih6prckja",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
 client.connect();
-
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-// const connectionString = 'postgres://deemmih6prckja:d4ed8ed76ccfe15034e68dc5c7f13423d10ba2598997673571023783923e0783@localhost:5433/pharmstore';
-// const client = new Client({
-//     connectionString: connectionString
-// });
-// client.connect();
 
 app.get('/store', function (req, res, next) {
   client.query('SELECT * FROM patients', function (err, result) {
